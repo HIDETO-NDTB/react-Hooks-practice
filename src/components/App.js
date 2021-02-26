1a3642a69af4004809e039cc715508bce7f8dbb5
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2021-02-26 17:08:34 +0900

    refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import React, { useReducer } from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import reducer from '../reducers';
-import Event from './Event';
 import EventForm from './EventForm';
+import Events from './Events';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
@@ -12,22 +10,7 @@ const App = () => {
   return (
     <div className="container-fluid">
       <EventForm state={state} dispatch={dispatch} />
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディ</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {state.map((event) => (
-            <Event key={event.id} event={event} dispatch={dispatch}/>
-          ))}
-        </tbody>
-      </table>
+      <Events state={state} dispatch={dispatch} />
     </div>
   );
 }
