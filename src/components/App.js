8a4fdbf5bdc0450f233b12b9b37aad2d6039408e
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2021-02-26 14:50:34 +0900

    implement all events deletion feature

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,15 @@ const App = () => {
     setBody('');
   };
 
+  const deleteAllEvents = e => {
+    e.preventDefault();
+    if (window.confirm('全てのイベントを削除してよろしいですか？')) {
+      dispatch({
+        type: 'DELETE_ALL_EVENTS'
+      })
+    }
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -35,8 +44,8 @@ const App = () => {
           <textarea className="form-control" id="formEventBody" onChange={e => setBody(e.target.value)} value={body} />
         </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent} disabled={ !(title && body) }>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={ state.length===0 }>全てのイベントを削除する</button>
       </form>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
--- src/components/Event.js
+++ src/components/Event.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Event = ({ event, dispatch }) => {
   const id = event.id;
   const handleClickDeleteButton = () => {
-    dispatch({
-      type: 'DELETE_EVENT',
-      id
-    })
+    if (window.confirm(`イベント（id:${id}）を削除してよろしいですか？`)) {
+      dispatch({
+        type: 'DELETE_EVENT',
+        id
+      })
+    }
   };
   return (
     <tr>
